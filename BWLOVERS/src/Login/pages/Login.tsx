27aef111f40dfdeec798060f2fc33a05f212ae07
wys0530 <youn.soo.woo@gmail.com>
2026-01-15 04:21:20 +0900

    feat:랜딩 페이지 퍼블리싱

--- BWLOVERS/src/Login/pages/Login.tsx
+++ BWLOVERS/src/Login/pages/Login.tsx
@@ -1,3 +1,19 @@
+import LoginBtn from '@/assets/Login/btn_naverlogin.svg?react';
+import LogoFull from '@/assets/common/logo_full.svg?react';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <div className="mb-34">
+        <LogoFull className="mb-2 h-[8.94081rem] w-53.75" />
+        <div className="flex h-7.25 justify-center text-heading-sm text-[#000000]">
+          태아보험, 이젠 더 쉽게!
+        </div>
+      </div>
+      <LoginBtn className="h-13.5 w-59.5 cursor-pointer" />
+      <div className="mt-5 cursor-pointer text-caption-md text-gray-60">
+        문의하기
+      </div>
+    </div>
+  );
 }
