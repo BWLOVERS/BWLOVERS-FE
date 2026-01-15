d30705fd08234fe3d6ee83347988ac32a87acc18
wys0530 <youn.soo.woo@gmail.com>
2026-01-15 14:21:21 +0900

    feat: 문의하기 말풍선 추가

--- BWLOVERS/src/Login/pages/Login.tsx
+++ BWLOVERS/src/Login/pages/Login.tsx
@@ -1,9 +1,17 @@
 import LoginBtn from '@/assets/Login/btn_naverlogin.svg?react';
 import LogoFull from '@/assets/common/logo_full.svg?react';
+import ContactBubble from '../components/contactBubble';
+
+import { useState } from 'react';
 
 export default function Login() {
+  const [isContactOpen, setIsContactOpen] = useState(false);
+
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
+    <div
+      className="flex flex-1 flex-col items-center justify-center"
+      onClick={() => setIsContactOpen(false)}
+    >
       <div className="mb-34">
         <LogoFull className="mb-2 h-[8.94081rem] w-53.75" />
         <div className="flex h-7.25 justify-center text-heading-sm text-[#000000]">
@@ -11,8 +19,25 @@ export default function Login() {
         </div>
       </div>
       <LoginBtn className="h-13.5 w-59.5 cursor-pointer" />
-      <div className="mt-5 cursor-pointer text-caption-md text-gray-60">
-        문의하기
+      <div className="relative">
+        <div
+          className={`${isContactOpen ? 'text-black' : 'text-gray-60'} mt-5 cursor-pointer text-caption-md transition hover:text-pink-80`}
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsContactOpen((prev) => !prev);
+          }}
+        >
+          문의하기
+        </div>
+
+        {isContactOpen && (
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="absolute left-1/2 mt-7 -translate-x-1/2"
+          >
+            <ContactBubble />
+          </div>
+        )}
       </div>
     </div>
   );
